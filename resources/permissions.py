cfe0020e54fb5551234075bbda226761b7bfd62e
EC2 Default User <ec2-user@ip-10-16-10-67.ec2.internal>
2023-09-25 03:37:53 +0000

    working on the lab

--- resources/permissions.py
+++ resources/permissions.py
@@ -2,7 +2,7 @@ import boto3
 import json
 
 S3API = boto3.client("s3", region_name="us-east-1") 
-bucket_name = "<FMI>"
+bucket_name = "c81790a1735454l4825114t1w336573178886-s3bucket-1qrmrarpsbwiz"
 
 policy_file = open("/home/ec2-user/environment/resources/public_policy.json", "r")
 
--- resources/website/config.js
+++ resources/website/config.js
@@ -1,4 +1,4 @@
 window.COFFEE_CONFIG = {
-	API_GW_BASE_URL_STR: null,
+	API_GW_BASE_URL_STR: "https://kyz7jbsap5.execute-api.us-east-1.amazonaws.com/prod",
 	COGNITO_LOGIN_BASE_URL_STR: null
 };
